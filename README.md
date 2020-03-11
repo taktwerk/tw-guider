9ad8e42765651e6a575f0ff89819fe3a31f4ff93
Konstantin Tretyak <tretyak_valant@bitbucket.org>
2020-03-11 18:43:12 +0200

    TWGU-7 - Ionic App - fixed problem of pressing the Back button during the full-screen mode of the video tag

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Execute follow commands from example:
 
     cp resources/android/xml/network_security_config.example.xml resources/android/xml/network_security_config.xml
 Add in created file subdomains (includeSubdomains)
+
+For Android platform:
+
+The solution to the problem of pressing the "Back" button during the full-screen mode of the video tag.
+
+in file platforms/android/app/src/main/java/com/taktwerk/twguider2/MainActivity.java add following code:
+
+```
+@Override
+public void onBackPressed() {
+    this.appView.loadUrl("javascript:if (document.webkitIsFullScreen === true) {document.webkitExitFullscreen();}");
+}
+```
--- src/components/modals/video-modal-component/video-modal-component.ts
+++ src/components/modals/video-modal-component/video-modal-component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, NgZone, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ModalController, Platform} from '@ionic/angular';
 import {ToastService} from '../../../services/toast-service';
 import {StreamingMedia, StreamingVideoOptions} from '@ionic-native/streaming-media/ngx';
@@ -18,17 +18,21 @@ import {DownloadService} from '../../../services/download-service';
   styleUrls: ['video-modal-component.scss']
 })
 
-export class VideoModalComponent implements OnInit {
+export class VideoModalComponent implements OnInit, OnDestroy {
     @ViewChild('video', {static: false}) video: ElementRef; // binds to #video in video.html
     videoElement: HTMLVideoElement;
     @Input() fileUrl: string;
     @Input() fileTitle: string;
+    backButtonSubscribe;
 
     constructor(private modalController: ModalController,
                 private platform: Platform,
                 private toastService: ToastService,
                 private streamingMedia: StreamingMedia,
-                private downloadService: DownloadService) {}
+                private downloadService: DownloadService,
+                private ngZone: NgZone) {
+        document['ionicComponentRef'] = {name: 'video-modal-component', component: this, zone: ngZone};
+    }
 
     dismiss() {
         this.modalController.dismiss();
@@ -38,6 +42,11 @@ export class VideoModalComponent implements OnInit {
         try {
             this.videoElement = this.video.nativeElement;
             this.videoElement.src = this.downloadService.getWebviewFileSrc(this.fileUrl);
+            this.videoElement.addEventListener('webkitfullscreenchange', () => {
+                if (!document['webkitIsFullScreen']) {
+                    this.dismiss();
+                }
+            });
             await this.videoElement.play();
         } catch (e) {
             this.openVideoViaMediaStreamingPlugin(this.fileUrl);
@@ -66,5 +75,23 @@ export class VideoModalComponent implements OnInit {
             shouldAutoClose: false
         };
         this.streamingMedia.playVideo(fileUrl, options);
+    }
+
+    backbuttonAction() {
+        this.dismiss();
+    }
+
+    ngOnInit() {
+        this.platform.ready().then(async () => {
+            this.backButtonSubscribe = this.platform.backButton.subscribeWithPriority(9999, () => {
+                document.addEventListener('backbutton', () => this.backbuttonAction());
+            });
+        });
+    }
+
+    ngOnDestroy() {
+        document['ionicComponentRef'] = null;
+        this.backButtonSubscribe.unsubscribe();
+        document.removeEventListener('backbutton', () => {});
     }
 }
