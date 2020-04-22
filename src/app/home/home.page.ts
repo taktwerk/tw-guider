dd17afea1a4cbe5827ec41032273af63bad3dbe5
Konstantin Tretyak <tretyak_valant@bitbucket.org>
2020-04-22 15:16:19 +0300

    TWGU-7 - Ionic App - QR Scanner should close on config error, as it wont scan a second time

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -67,36 +67,43 @@ export class HomePage {
               this.closeScanner();
               return;
             } else {
-              const user = await this.userService.getUser();
-              if (user) {
-                await this.authService.logout();
-              }
-              const appConfirmUrl =  config.host + environment.apiUrlPath + '/login/';
-              if (config.mode === AppConfigurationModeEnum.CONFIGURE_AND_DEVICE_LOGIN && config.clientIdentifier) {
-                await this.authService.loginByIdentifier(appConfirmUrl, 'client', config.clientIdentifier);
-              } else if (config.mode === AppConfigurationModeEnum.CONFIGURE_AND_USER_LOGIN && config.userIdentifier) {
-                await this.authService.loginByIdentifier(appConfirmUrl, 'user', config.userIdentifier);
-              }
-              config.isWasQrCodeSetup = true;
-              this.appSetting.save(config).then(() => {
-                this.appSetting.isWasQrCodeSetupSubscribtion.next(true);
-                this.userService.getUser().then(loggedUser => {
-                  const isUserLoggedIn = !!loggedUser;
-                  if (!isUserLoggedIn) {
-                    this.ngZone.run(() => {
-                      this.navCtrl.navigateRoot('/login');
-                    });
-                  } else {
-                    this.events.publish('qr-code:setup');
-                    this.ngZone.run(() => {
-                      this.navCtrl.navigateRoot('/guides');
-                    });
-                  }
-                });
+              try {
+                const user = await this.userService.getUser();
+                if (user) {
+                  await this.authService.logout();
+                }
+                const appConfirmUrl =  config.host + environment.apiUrlPath + '/login/';
+                if (config.mode === AppConfigurationModeEnum.CONFIGURE_AND_DEVICE_LOGIN && config.clientIdentifier) {
+                  console.log('clientIdentifier');
+                  await this.authService.loginByIdentifier(appConfirmUrl, 'client', config.clientIdentifier);
+                } else if (config.mode === AppConfigurationModeEnum.CONFIGURE_AND_USER_LOGIN && config.userIdentifier) {
+                  console.log('userIdentifier');
+                  await this.authService.loginByIdentifier(appConfirmUrl, 'user', config.userIdentifier);
+                }
+                config.isWasQrCodeSetup = true;
+                this.appSetting.save(config).then(() => {
+                  this.appSetting.isWasQrCodeSetupSubscribtion.next(true);
+                  this.userService.getUser().then(loggedUser => {
+                    const isUserLoggedIn = !!loggedUser;
+                    if (!isUserLoggedIn) {
+                      this.ngZone.run(() => {
+                        this.navCtrl.navigateRoot('/login');
+                      });
+                    } else {
+                      this.events.publish('qr-code:setup');
+                      this.ngZone.run(() => {
+                        this.navCtrl.navigateRoot('/guides');
+                      });
+                    }
+                  });
 
-                this.http.showToast('qr.Application was successfully configured');
-              });
-              this.closeScanner();
+                  this.http.showToast('qr.Application was successfully configured');
+                });
+                this.closeScanner();
+              } catch (e) {
+                console.log('This is error', e);
+                this.closeScanner();
+              }
             }
           });
         })
