b75bb691597a5195a4f3dfe0994847822662f724
Konstantin Tretyak <kons1tretyak1valant@gmail.com>
2020-09-24 18:48:45 +0300

    TWGU-7 - Ionic App - fix bug of the building on iOS

--- scripts/before_platform_add.js
+++ scripts/before_platform_add.js
@@ -1,10 +1,8 @@
 var exec = require('child_process').exec, child;
 
 module.exports = function(ctx) {
-    return new Promise(async resolve => {
-        exec('ionic cordova plugin add https://github.com/moust/cordova-plugin-videoplayer.git --no-save', function (error, stdout, stderr) {
-            console.log('add plugin com.moust.cordova.videoplayer');
-        });
-    }).then(msWaited => {});
+    exec('ionic cordova plugin add https://github.com/moust/cordova-plugin-videoplayer.git --no-save', function (error, stdout, stderr) {
+        console.log('add plugin com.moust.cordova.videoplayer');
+    });
 };
 
