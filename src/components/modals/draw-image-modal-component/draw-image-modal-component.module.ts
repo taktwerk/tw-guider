005c6eb35c3df30533280c2e05efbc9ecf27cd8e
Konstantin Tretyak <kons1tretyak1valant@gmail.com>
2020-10-26 15:16:18 +0200

    TWGU-15 - Protocol - fix prod build bug

--- src/components/modals/draw-image-modal-component/draw-image-modal-component.module.ts
+++ src/components/modals/draw-image-modal-component/draw-image-modal-component.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
-import {DrawImageModalComponent} from './draw-image-modal-component';
+import {TranslateModule} from '@ngx-translate/core';
+
 
 @NgModule({
-  declarations: [
-    DrawImageModalComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     IonicModule,
+    TranslateModule
   ],
-  exports: [
-    DrawImageModalComponent
-  ],
+  exports: []
 })
 export class DrawImageModalComponentModule {}
