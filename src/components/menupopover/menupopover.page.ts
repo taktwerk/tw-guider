f8c30037b8ece94a5c45daeede545f82108d671f
easyadin <tobiayokunnu@gmail.com>
2020-11-26 22:56:33 +0400

    Update menupopover.page.ts

--- src/components/menupopover/menupopover.page.ts
+++ src/components/menupopover/menupopover.page.ts
@@ -1,3 +1,5 @@
+import { GuideinfoPage } from 'src/components/guideinfo/guideinfo.page';
+import { ModalController, NavParams } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ApiSync } from 'src/providers/api-sync';
 
@@ -7,8 +9,29 @@ import { ApiSync } from 'src/providers/api-sync';
   styleUrls: ['./menupopover.page.scss'],
 })
 export class MenuPopoverComponent implements OnInit {
-  constructor(private apiSync: ApiSync) { }
+  guideId;
+  constructor(
+    private apiSync: ApiSync,
+    private modalController: ModalController,
+    public navParams: NavParams
+  ) {
+    this.guideId = this.navParams.get('guideId');
+  }
 
   ngOnInit() { }
 
+  async presentGuideInfo() {
+    const modal = await this.modalController.create({
+      component: GuideinfoPage,
+      componentProps: {
+        'guideId': this.guideId
+      },
+      cssClass: "modal-fullscreen"
+    });
+    return await modal.present();
+  }
+
+  async restartSlide(ev: any) {
+    
+   }
 }
