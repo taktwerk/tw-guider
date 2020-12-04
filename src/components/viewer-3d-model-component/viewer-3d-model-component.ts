0ea63273f7bce577afdff5dede8e1459a9e9b284
easyadin <tobiayokunnu@gmail.com>
2020-12-04 17:38:04 +0400

    gltfError Error: THREE.GLTFLoader: Unsupported asset. glTF versions >=2.0 are supported. Use LegacyGLTFLoader instead.

--- src/components/viewer-3d-model-component/viewer-3d-model-component.ts
+++ src/components/viewer-3d-model-component/viewer-3d-model-component.ts
@@ -11,17 +11,17 @@ import {
 import { Capacitor, Plugins, CameraResultType, FilesystemDirectory } from '@capacitor/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
-import {GLTFExporter} from "three/examples/jsm/exporters/GLTFExporter";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
 import * as skeletonUtils from "three/examples/jsm/utils/SkeletonUtils";
 import { File } from '@ionic-native/file/ngx';
 import { AfterViewInit } from '@angular/core';
 import { NgZone } from '@angular/core';
 import { createGesture, Gesture, GestureDetail } from '@ionic/core';
 import { Storage } from '@ionic/storage';
-import {Viewer3dService} from "../../services/viewer-3d-service";
+import { Viewer3dService } from "../../services/viewer-3d-service";
 
 /**
  * Generated class for the TodoPage page.
@@ -30,12 +30,12 @@ import {Viewer3dService} from "../../services/viewer-3d-service";
  * on Ionic pages and navigation.
  */
 
- const { Filesystem } = Plugins;
+const { Filesystem } = Plugins;
 
 @Component({
-  selector: 'viewer-3d-model-component',
-  styleUrls: ['viewer-3d-model-component.scss'],
-  templateUrl: 'viewer-3d-model-component.html',
+    selector: 'viewer-3d-model-component',
+    styleUrls: ['viewer-3d-model-component.scss'],
+    templateUrl: 'viewer-3d-model-component.html',
 })
 export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
     @Input() fileName: string;
@@ -44,7 +44,7 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
     @Input() willStopRotate = true;
     @Input() fullScreen = true;
 
-    @ViewChild('domObj', {static: false}) domObj: ElementRef;
+    @ViewChild('domObj', { static: false }) domObj: ElementRef;
 
     isInit = false;
     isRendered = false;
@@ -72,7 +72,7 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         private changeDetectorRef: ChangeDetectorRef,
         private storage: Storage,
         private viewer3d: Viewer3dService
-    ) {}
+    ) { }
 
     async init() {
         if (this.isInit) {
@@ -94,7 +94,6 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         this.ctx = document.createElement('canvas').getContext('2d');
         this.modelElement.appendChild(this.ctx.canvas);
         ///new
-
         await this.renderModels();
     }
 
@@ -128,37 +127,60 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         const path = this.fileName.slice(0, (fileName.length) * -1);
         let bufferData = null;
         try {
-            console.log('pathfileName', (path + fileName));
-            const modelFile = await Filesystem.readFile({ path: path + fileName });
-            var binary_string = window.atob(modelFile.data);
-            var len = binary_string.length;
-            var bytes = new Uint8Array(len);
-            for (var i = 0; i < len; i++) {
-                bytes[i] = binary_string.charCodeAt(i);
+            const path__fileName = path + fileName;
+            if (path__fileName.split('.').pop() == "gltf") {
+                console.log('pathfileName', (path + fileName));
+                // read model from path
+                // const modelFile = await Filesystem.readFile({ path: path + fileName });
+                const convertFileSrc = Capacitor.convertFileSrc(path + fileName);
+                const response = await fetch(convertFileSrc);
+                let modelFile = await response.json();
+                console.log(modelFile)
+
+                modelFile = modelFile.buffers[0].uri;
+                modelFile = modelFile.replace("data:application/octet-stream;base64,", "")
+                var binary_string = window.atob(modelFile);
+                console.log(binary_string)
+                var len = binary_string.length;
+                console.log(len)
+                var bytes = new Uint8Array(len);
+                console.log(bytes)
+                for (var i = 0; i < len; i++) {
+                    bytes[i] = binary_string.charCodeAt(i);
+                }
+                bufferData = bytes.buffer;
+                console.log(bufferData)
             }
-            bufferData = bytes.buffer;
+
         } catch (error) {
             console.log('3d model file error', error);
         }
 
-        const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath( '/assets/threeJs/loaders/gltf/draco/' );
-        dracoLoader.setDecoderConfig({type: 'js'});
-        loader.setDRACOLoader( dracoLoader )
-        loader.parse(bufferData, '', (gltf) => {
-                this.gltfScene = gltf.scene;
-                THREE.Cache.enabled = true;
-                THREE.Cache.add(this.fileName, skeletonUtils.SkeletonUtils.clone(gltf.scene));
-                THREE.Cache.enabled = false;
-                const sceneSkeleton: any = skeletonUtils.SkeletonUtils.clone(gltf.scene);
-                this.storage.set(this.fileName, sceneSkeleton.toJSON());
+        if (bufferData != null) {
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath('/assets/threeJs/loaders/gltf/draco/');
+            dracoLoader.setDecoderConfig({ type: 'js' });
+            loader.setDRACOLoader(dracoLoader);
+            console.log(bufferData)
+            const dataString = JSON.stringify(Array.from(new Uint8Array(bufferData)));
+            console.log(dataString)
+            loader.parse(dataString, '',
+                (gltf) => {
+                    this.gltfScene = gltf.scene;
+                    console.log(this.gltfScene)
+                    THREE.Cache.enabled = true;
+                    // THREE.Cache.add(this.fileName, skeletonUtils.SkeletonUtils.clone(gltf.scene));
+                    THREE.Cache.enabled = false;
+                    // const sceneSkeleton: any = skeletonUtils.SkeletonUtils.clone(gltf.scene);
+                    // this.storage.set(this.fileName, sceneSkeleton.toJSON());
 
-                this.renderModel();
-            },
-            (error) => {
-                console.log('gltfError', error);
-            }
-        );
+                    this.renderModel();
+                },
+                (error) => {
+                    console.log('gltfError', error);
+                }
+            );
+        }
     }
 
     render() {
@@ -169,32 +191,32 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         if (this.stopRender) {
             return;
         }
-        const {left, right, top, bottom, width, height} = this.modelElement.getBoundingClientRect();
+        const { left, right, top, bottom, width, height } = this.modelElement.getBoundingClientRect();
         if (this.resizeCanvas) {
             this.camera.aspect = width / height;
             this.camera.updateProjectionMatrix();
-            this.renderer.setSize( this.modelElement.width, this.modelElement.height, false );
+            this.renderer.setSize(this.modelElement.width, this.modelElement.height, false);
         }
 
         /// new
-         const isOffscreen =
+        const isOffscreen =
             bottom < 0 ||
             top > window.innerHeight ||
             right < 0 ||
             left > window.innerWidth;
 
-        if (isOffscreen) { 
-          this.requestAnimationFrameId = requestAnimationFrame(() => {
-              this.animate();
-          });
-          return;
+        if (isOffscreen) {
+            this.requestAnimationFrameId = requestAnimationFrame(() => {
+                this.animate();
+            });
+            return;
         }
 
         const rendererCanvas = this.renderer.domElement;
         this.renderer.setSize(width, height, false);
         if (this.ctx.canvas.width !== width || this.ctx.canvas.height !== height) {
-          this.ctx.canvas.width = width;
-          this.ctx.canvas.height = height;
+            this.ctx.canvas.width = width;
+            this.ctx.canvas.height = height;
         }
         this.renderer.setScissor(0, 0, width, height);
         this.renderer.setViewport(0, 0, width, height);
@@ -216,17 +238,17 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         /// new
         this.requestAnimationFrameId = requestAnimationFrame(() => {
             this.animate();
-        });        
+        });
     }
 
     renderModel() {
         this.renderer = this.viewer3d.getGlobalRenderer();
         const object: any = this.gltfScene;
         this.pivot = new THREE.Group();
-        const light1  = new THREE.AmbientLight(0xffffff, 3);
-        const light2  = new THREE.DirectionalLight(0xffffff, 4);
+        const light1 = new THREE.AmbientLight(0xffffff, 3);
+        const light2 = new THREE.DirectionalLight(0xffffff, 4);
         light2.position.set(0.5, 0, 0.866); // ~60º
-        this.pivot.add( object );
+        this.pivot.add(object);
         this.scene = new THREE.Scene();
         this.scene.add(light1);
         this.scene.add(light2);
@@ -234,14 +256,14 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         if (this.backgroundColor) {
             this.scene.background = new THREE.Color(this.backgroundColor);
         }
-        const box = new THREE.Box3().setFromObject( object );
-        const center = box.getCenter( object.position );
-        
+        const box = new THREE.Box3().setFromObject(object);
+        const center = box.getCenter(object.position);
+
         // object.position.x += (object.position.x - center.x);
         // object.position.y += (object.position.y - center.y);
         // object.position.z += (object.position.z - center.z);
 
-        const {left, right, top, bottom, width, height} = this.modelElement.getBoundingClientRect();
+        const { left, right, top, bottom, width, height } = this.modelElement.getBoundingClientRect();
         const areaWidth = width;
         const areaHeight = height;
 
@@ -251,31 +273,31 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         console.log('height', height)
 
         this.camera = new THREE.PerspectiveCamera(
-                    50,
-                    width/height,
-                    size / 100,
-                    size * 100
-                );
+            50,
+            width / height,
+            size / 100,
+            size * 100
+        );
         // this.camera.position.copy(center);
         this.camera.position.x += size;
         this.camera.position.y += size / 5.0;
         this.camera.position.z = (2 + size / 2.0);
         this.camera.lookAt(center);
-        
+
 
         // this.pivot.add(this.camera);
         // this.camera.lookAt( this.pivot.position );
 
         this.scene.add(this.camera);
 
-        object.position.multiplyScalar( - 1 );
+        object.position.multiplyScalar(- 1);
 
         this.camera.updateProjectionMatrix();
 
         this.controls = new OrbitControls(this.camera, this.modelElement);
         this.controls.enableRotate = this.madeUserIteractions;
         this.controls.enablePan = false;
-        
+
         this.ngZone.runOutsideAngular(() => {
             this.animate();
         });
@@ -317,33 +339,32 @@ export class Viewer3dModelComponent implements AfterViewChecked, OnDestroy {
         this.stopRender = true;
     }
 
-  cancelRender() {
+    cancelRender() {
         if (this.requestAnimationFrameId) {
-          cancelAnimationFrame(this.requestAnimationFrameId);
+            cancelAnimationFrame(this.requestAnimationFrameId);
         }
-  }
+    }
 
-  stopAutoRotation() {
+    stopAutoRotation() {
         if (this.isRotateModel) {
             this.isRotateModel = false;
         }
-  }
+    }
 
-  ngAfterViewChecked()
-  {
-      if (!this.isInit) {
-          this.ngZone.runOutsideAngular(() => {
-              this.init();
-          });
-      }
-  }
+    ngAfterViewChecked() {
+        if (!this.isInit) {
+            this.ngZone.runOutsideAngular(() => {
+                this.init();
+            });
+        }
+    }
 
-  ngOnDestroy() {
-      this.stopRender = true;
-      if (this.requestAnimationFrameId) {
-          cancelAnimationFrame(this.requestAnimationFrameId);
-      }
-  }
+    ngOnDestroy() {
+        this.stopRender = true;
+        if (this.requestAnimationFrameId) {
+            cancelAnimationFrame(this.requestAnimationFrameId);
+        }
+    }
 
     detectChanges() {
         if (!this.changeDetectorRef['destroyed']) {
