41971bd26faaedcd56098868820ad43265709efa
easyadin <tobiayokunnu@gmail.com>
2020-12-14 18:29:04 +0400

    Update safe-html.ts

--- src/pipes/safe-html/safe-html.ts
+++ src/pipes/safe-html/safe-html.ts
@@ -10,7 +10,7 @@ export class SafeHtmlPipe implements PipeTransform {
         // const unwanted = ['<th>', '<tr>', '<td>', '<li>', '<ol>', '<p>', '<h1>', '<h2>', '<h3>', '<h4>', '<h5>', '<h6>', '<blockquote>', '</th>', '</tr>', '</td>', '</li>', '</ol>', '</p>', '</h1>', '</h2>', '</h3>', '</h4>', '</h5>', '<h6>', '</blockquote>']
         if (slice) {
             // unwanted.map(u => value.replace(u, ''))
-            value = value.length >= this.limit ? value.slice(0, 19) + '...' : value.slice(0, 19);
+            value = value.length >= this.limit ? value.slice(0, this.limit) + '...' : value.slice(0, 19);
 
             return this.sanitized.bypassSecurityTrustHtml(value);
         }
