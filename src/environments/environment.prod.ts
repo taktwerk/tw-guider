29e8cddde98625ec93db060700d609a3e81844a2
easyadin <tobiayokunnu@gmail.com>
2021-02-02 23:56:41 +0400

    added env variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import { NgxLoggerLevel } from "ngx-logger";
+
+export const environment = {
+  production: true,
+  apiUrlPath: '/api/v1',
+  taktwerk: 'taktwerk',
+  dbName: 'guider',
+  dbMigrationVersion: '2',
+  usbHost: 'https://192.168.42.42:4430',
+  sentryDsn: 'https://06e02ze76028a24c4eacf3f1018809916c@sentry.io/1398173',
+  pspdfkitIosLicenseKey: 'Y4QP5QA1LluD7MgTa37BGc3pjoV8BbR9Xa5yt2zltueCboYYMUV5PzLODzHHnAzN5VC6dZY4JumqDOaD2AzNTxZyN7Q5ll5HWmTaqF4PzWGwwcxgrno7U6sFhA9rr06vYqHM2rPe3Ht3am+0iTizjb4hQFyixTo1NkRJfvNZOebniUgN1r8cPB4QEBNyixYpbk8G075JY4/NBrJg3WkKerJyPV5WKD/in7IXJlqNWYllc+7JhwB9pzJ8G8EQQ8x2lQMJNEnppAqIP5mVTGC+mnlGmrcEPSNOMOcFMnuaXNnhksEuQegltYGSD96O9smozPbn3yudBBUOWdvsbuA0gsy3UYYfM2Fs3pd1z3boypZjK9OoZhY1AJBGHmp5KL67+B+lNkO5hhT3FyG4Dap4JpBOs3r6Mv6laeBuW6fkgxlilU9JYitM1XuUPiTlyVR+',
+  logger: {
+    level: NgxLoggerLevel.DEBUG,
+    serverLogLevel: NgxLoggerLevel.ERROR,
+    serverLoggerUrl: 'https://taktwerkloggertest.free.beeceptor.com/logs'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
