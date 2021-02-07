be89e1739cbf2961209134eb04d0635d21c8cd50
Cedric Steiner <c.steiner@taktwerk.ch>
2021-02-07 15:39:23 +0100

    add/update build/bundle scripts

--- d/bundle.sh
+++ d/bundle.sh
@@ -1,5 +1,5 @@
-cd platforms/android
-./gradlew bundle
-cd ../../
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore '/media/cedric/DATEN/CLOUDS/taktwerk/11 Infrastruktur/05 DevEnv/Keys/taktwerk_upload_keystore.pkcs.jks' platforms/android/app/build/outputs/bundle/release/app.aab upload
-cp platforms/android/app/build/outputs/bundle/release/app.aab app.aab
+cd android
+sudo ./gradlew bundle
+cd ../
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore '/media/cedric/DATEN/CLOUDS/taktwerk/11 Infrastruktur/05 DevEnv/Keys/taktwerk_upload_keystore.pkcs.jks' android/app/build/outputs/bundle/release/app-release.aab upload
+cp android/app/build/outputs/bundle/release/app-release.aab app.aab
