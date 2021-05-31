6f372a0f1298dbf4a740a0f7a9f8c09f4cfb5d56
developement0261 <developement0261@gmail.com>
2021-05-31 19:01:34 +0530

    Resolve bugs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import {AuthService} from '../../services/auth-service';
-import {HttpClient} from '../../services/http-client';
+import { AuthService } from '../../services/auth-service';
+import { HttpClient } from '../../services/http-client';
 import { NgForm } from '@angular/forms';
-import {ApiSync} from '../../providers/api-sync';
-import {Network} from '@ionic-native/network/ngx';
-import {AppSetting} from '../../services/app-setting';
+import { ApiSync } from '../../providers/api-sync';
+import { Network } from '@ionic-native/network/ngx';
+import { AppSetting } from '../../services/app-setting';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,106 +16,107 @@ import {AppSetting} from '../../services/app-setting';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.page.html',
-  styleUrls: ['login.page.scss']
+  styleUrls: ['login.page.scss'],
 })
-
 export class LoginPage {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
+  currDate: Date = new Date();
+  public center;
 
-    public center;
+  public params;
+  /** flag that indicates if the login was failed or not */
+  isLoginFailed: boolean = false;
 
-    public params;
-    /** flag that indicates if the login was failed or not */
-    isLoginFailed: boolean = false;
+  /**
+   * @param navCtrl
+   * @param authService
+   * @param http
+   * @param apiSync
+   * @param network
+   * @param appSetting
+   */
+  constructor(
+    public navCtrl: NavController,
+    public authService: AuthService,
+    public http: HttpClient,
+    public apiSync: ApiSync,
+    private network: Network,
+    private appSetting: AppSetting
+  ) {}
 
-    /**
-     * @param navCtrl
-     * @param authService
-     * @param http
-     * @param apiSync
-     * @param network
-     * @param appSetting
-     */
-    constructor(public navCtrl: NavController,
-                public authService: AuthService,
-                public http: HttpClient,
-                public apiSync: ApiSync,
-                private network: Network,
-                private appSetting: AppSetting) {}
-
-    /**
-     * Try to login with the current pin-code and redirect the user to the
-     * dashboard if the login was successful. Otherwise show fail message.
-     */
-    login(form: NgForm) {
-        if (!form.value.password && !form.value.username) {
-            this.http.showToast('validation.Username and password is required');
-            return;
-        }
-        if (!form.value.password) {
-            this.http.showToast('validation.Password is required');
-            return;
-        }
-        if (!form.value.username) {
-            this.http.showToast('validation.Username is required');
-            return;
-        }
-        if (this.network.type === 'none' && !this.appSetting.isEnabledUsb) {
-            this.loginOffine(form);
-        } else {
-            this.loginOnline(form);
-        }
+  /**
+   * Try to login with the current pin-code and redirect the user to the
+   * dashboard if the login was successful. Otherwise show fail message.
+   */
+  login(form: NgForm) {
+    if (!form.value.password && !form.value.username) {
+      this.http.showToast('validation.Username and password is required');
+      return;
     }
-
-    loginOffine(form: NgForm) {
-        this.authService.offlineAuthenticate(form.value).then((result) => {
-            if (!result) {
-                this.http.showToast('validation.Wrong password or login!');
-            } else {
-                this.navCtrl.navigateRoot('/guide-categories');
-                this.http.showToast('login.You are logged in.');
-            }
-        });
+    if (!form.value.password) {
+      this.http.showToast('validation.Password is required');
+      return;
     }
-
-    loginOnline(form: NgForm) {
-        this.authService.authenticate(form.value).then((result: number) => {
-            switch (result) {
-                case AuthService.STATE_ERROR_NETWORK:
-                    // network problem
-                    this.username = '';
-                    this.password = '';
-                    this.http.showToast();
-                    break;
-                case AuthService.STATE_ERROR_INVALID_LOGIN:
-                    // invalid login
-                    this.username = '';
-                    this.password = '';
-                    this.isLoginFailed = true;
-                    this.http.showToast('Wrong password!');
-                    break;
-                case AuthService.STATE_ERROR_USER_BLOCKED:
-                    this.username = '';
-                    this.password = '';
-                    this.isLoginFailed = true;
-                    this.http.showToast('validation.user_blocked');
-                    break;
-                case AuthService.STATE_ERROR_USER_CANT_LOGIN:
-                    this.username = '';
-                    this.password = '';
-                    this.isLoginFailed = true;
-                    this.http.showToast('validation.user_cant_login');
-                    break;
-                default:
-                    this.navCtrl.navigateRoot('/guide-categories');
-                    this.http.showToast('login.You are logged in.');
-            }
-        });
+    if (!form.value.username) {
+      this.http.showToast('validation.Username is required');
+      return;
     }
-
-    delete() {
-        this.username = '';
-        this.password = '';
+    if (this.network.type === 'none' && !this.appSetting.isEnabledUsb) {
+      this.loginOffine(form);
+    } else {
+      this.loginOnline(form);
     }
+  }
+
+  loginOffine(form: NgForm) {
+    this.authService.offlineAuthenticate(form.value).then((result) => {
+      if (!result) {
+        this.http.showToast('validation.Wrong password or login!');
+      } else {
+        this.navCtrl.navigateRoot('/guide-categories');
+        this.http.showToast('login.You are logged in.');
+      }
+    });
+  }
+
+  loginOnline(form: NgForm) {
+    this.authService.authenticate(form.value).then((result: number) => {
+      switch (result) {
+        case AuthService.STATE_ERROR_NETWORK:
+          // network problem
+          this.username = '';
+          this.password = '';
+          this.http.showToast();
+          break;
+        case AuthService.STATE_ERROR_INVALID_LOGIN:
+          // invalid login
+          this.username = '';
+          this.password = '';
+          this.isLoginFailed = true;
+          this.http.showToast('Wrong password!');
+          break;
+        case AuthService.STATE_ERROR_USER_BLOCKED:
+          this.username = '';
+          this.password = '';
+          this.isLoginFailed = true;
+          this.http.showToast('validation.user_blocked');
+          break;
+        case AuthService.STATE_ERROR_USER_CANT_LOGIN:
+          this.username = '';
+          this.password = '';
+          this.isLoginFailed = true;
+          this.http.showToast('validation.user_cant_login');
+          break;
+        default:
+          this.navCtrl.navigateRoot('/guide-categories');
+          this.http.showToast('login.You are logged in.');
+      }
+    });
+  }
+
+  delete() {
+    this.username = '';
+    this.password = '';
+  }
 }
