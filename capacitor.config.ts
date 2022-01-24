1eec824faa013df7c0ad1a373c46a87b6ce0e098
Cedric Steiner <c.steiner@taktwerk.ch>
2022-01-24 12:30:26 +0100

    Rebuild: New files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taktwerk.guider',
+  appName: 'Guider 2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
