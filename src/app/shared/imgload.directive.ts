8441024e170523a6f8bcb30daaf5c1c05d9e019f
Harjot Singh <harjotupwork08@gmail.com>
2022-05-23 14:23:41 +0530

    done

--- src/app/shared/imgload.directive.ts
+++ src/app/shared/imgload.directive.ts
@@ -21,20 +21,36 @@ export class ImgloadDirective implements OnInit, OnChanges {
     this.onLoadData();
 
   }
+  isImage(base64Data) {
+    if (base64Data == null) return false;
+
+    let mimeType = base64Data?.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/);
+    if (mimeType == null) return false;
+
+    mimeType = mimeType[0]?.split('/')[0];
+    if (mimeType == 'image') {
+      return true
+    }
+    return false
+  }
 
   onLoadData() {
 
-    if (this.platform.is('capacitor')) {
-      this.ele.nativeElement.src = this.url.changingThisBreaksApplicationSecurity;
-      return;
-    }
+    // if (this.platform.is('capacitor')) {
+    //   this.ele.nativeElement.src = this.url.changingThisBreaksApplicationSecurity;
+    //   return;
+    // }
 
     const blobToBase64 = (blob) => new Promise((resolve, _) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(blob);
-      });
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+    });
 
+    if (this.isImage(this.localurl)) {
+      this.ele.nativeElement.src = this.localurl;
+      return;
+    }
 
     if (this.localurl != null) {
       fetch(this.localurl).then(r => {
