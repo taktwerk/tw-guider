7e1b122fe29c35d1e75da401fce88eecbfa1ab80
Harjot <harjotupwork08@gmail.com>
2022-11-15 20:37:21 +0530

    setup controllers

---
+++ local-server/controllers/SettingController.ts
@@ -0,0 +1,6 @@
+export default class SettingController {
+  getSetting(data) {
+
+    return data;
+  }
+}
---
+++ local-server/routes/index.ts
@@ -0,0 +1,10 @@
+const routes: Array<any> = [
+  {
+      type: 'post',
+      path: '/register',
+      method: 'getSetting',
+      controller: 'SettingController'
+  }
+];
+
+export default routes;
