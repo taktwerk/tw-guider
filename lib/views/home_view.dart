5209e9b382d3789dd0affc34de9064c3846b60fb
Cedric Steiner <c.steiner@taktwerk.ch>
2023-07-19 15:03:24 +0200

    Add ListItem widget (instruction list with image)

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:guider/helpers/search.dart';
 import 'package:guider/objects/instruction.dart';
-import 'package:guider/views/instruction_view.dart';
 import 'package:guider/widgets/searchbar.dart';
-import 'package:provider/provider.dart';
+import 'package:guider/widgets/listitem.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
-
 }
 
 class ListOfInstructions extends ChangeNotifier {
   var list = [];
 }
 
-class _HomeState extends State<Home>
-with AutomaticKeepAliveClientMixin<Home> {
+class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
   List<InstructionElement>? _instructions;
 
   Future getAllInstructions() async {
@@ -46,47 +43,20 @@ with AutomaticKeepAliveClientMixin<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          children: [
-          SearchBarWidget(updateInstructions: updateInstructions),
-          _instructions != null
-          ? Expanded(child: 
-          ListView.builder(
-            itemCount: _instructions!.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: SizedBox(
-                  height: 100,
-                  child: ListTile(    
-                      leading: ConstrainedBox(
-                        constraints: const BoxConstraints(
-                          minWidth: 100,
-                          minHeight: 120,
-                          maxWidth: 200,
-                          maxHeight: 120,
-                        ),
-                        child: Image.network(_instructions![index].image),
-                      ),
-
-                        title: Text(_instructions![index].title),
-                        subtitle: Text(_instructions![index].shortTitle),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => InstructionView(instruction: _instructions![index])),
-                          );
-                        },
-                        trailing: const Icon(Icons.arrow_forward_ios),
-                      ),
-                      ),
-              );
-            },)
-          ,)
-          :Container(),
-        ],
-
-      )
-
-    );
+        body: Column(
+      children: [
+        SearchBarWidget(updateInstructions: updateInstructions),
+        _instructions != null
+            ? Expanded(
+                child: ListView.builder(
+                  itemCount: _instructions!.length,
+                  itemBuilder: (context, index) {
+                    return ListItem(instruction: _instructions![index]);
+                  },
+                ),
+              )
+            : Container(),
+      ],
+    ));
   }
 }
---
+++ lib/widgets/listitem.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:guider/objects/instruction.dart';
+import 'package:guider/views/instruction_view.dart';
+
+class ListItem extends StatelessWidget {
+  const ListItem({
+    super.key,
+    required InstructionElement instruction,
+  }) : _instruction = instruction;
+
+  final InstructionElement _instruction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      InstructionView(instruction: _instruction)),
+            );
+          },
+          child: SizedBox(
+              height: 150,
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 20,
+                          child: Container(
+                              alignment: Alignment.topLeft,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Image.network(
+                                  _instruction.image,
+                                  height: 150,
+                                  width: 250,
+                                  fit: BoxFit.cover,
+                                ),
+                              ))),
+                      const Spacer(flex: 2),
+                      Expanded(
+                        flex: 50,
+                        child: Column(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Container(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            7, 2, 7, 2),
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey.shade400,
+                                            border: Border.all(
+                                                color: Colors.grey.shade400),
+                                            borderRadius:
+                                                const BorderRadius.all(
+                                                    Radius.circular(20))),
+                                        child: Text(
+                                          _instruction.shortTitle,
+                                          style: const TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.white),
+                                        )))),
+                            Expanded(
+                              flex: 2,
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(_instruction.title,
+                                      style: const TextStyle(fontSize: 23))),
+                            ),
+                            const Expanded(
+                              flex: 1,
+                              child: Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Text('Steps: ? | Last uupdate: ?',
+                                      style: TextStyle(color: Colors.grey))),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Icon(Icons.arrow_forward_ios)
+                    ],
+                  )))),
+    );
+  }
+}
