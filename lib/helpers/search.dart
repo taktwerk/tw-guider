aa495377bac232d668c35a87abe7151aa17f80e8
ksiarsky <k.siarsky@taktwerk.ch>
2023-07-20 14:16:25 +0200

    Add of dropdown menu and buttons to switch steps

--- lib/helpers/search.dart
+++ lib/helpers/search.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:guider/objects/instruction.dart';
 import 'package:guider/objects/instruction_steps.dart';
 
 class Search {
   static Future<List<InstructionElement>> getAllInstructions() async {
-    final String response = await rootBundle.loadString('assets/jsons/instruction.json');
+    final String response =
+        await rootBundle.loadString('assets/jsons/instruction.json');
     final Instructions instructions = instructionFromJson(response);
     return instructions.instructionElements;
   }
@@ -14,31 +14,35 @@ class Search {
     // TODO: take from localstorage
     var allInstructions = await getAllInstructions();
     final result = <InstructionElement>[];
-    for(var item in allInstructions) {
-      if(myFilter(item, value)) {
+    for (var item in allInstructions) {
+      if (myFilter(item, value)) {
         result.add(item);
       }
     }
     print("Result length ${result.length}");
     return result;
-    }
+  }
 
   static bool myFilter(item, value) {
     var lowercaseItemTitle = item.title.toLowerCase();
     var lowercaseItemShorttitle = item.shortTitle.toLowerCase();
     var lowercaseValue = value.toLowerCase();
-    if(lowercaseItemTitle.contains(lowercaseValue) || lowercaseItemShorttitle.contains(lowercaseValue)) {
+    if (lowercaseItemTitle.contains(lowercaseValue) ||
+        lowercaseItemShorttitle.contains(lowercaseValue)) {
       return true;
     }
     return false;
   }
 
-  static Future<List<InstructionStep>> getInstructionSteps(instructionId) async {
-    final String response = await rootBundle.loadString('assets/jsons/instruction_step.json');
-    final InstructionSteps instructionsteps = instructionStepsFromJson(response);
+  static Future<List<InstructionStep>> getInstructionSteps(
+      instructionId) async {
+    final String response =
+        await rootBundle.loadString('assets/jsons/instruction_step.json');
+    final InstructionSteps instructionsteps =
+        instructionStepsFromJson(response);
     final filteredList = <InstructionStep>[];
-    for(var step in instructionsteps.instructionSteps) {
-      if(filterByInstructionId(step, instructionId)) {
+    for (var step in instructionsteps.instructionSteps) {
+      if (filterByInstructionId(step, instructionId)) {
         filteredList.add(step);
       }
     }
@@ -47,7 +51,7 @@ class Search {
   }
 
   static bool filterByInstructionId(step, instructionId) {
-    if(step.instructionId == instructionId) {
+    if (step.instructionId == instructionId) {
       return true;
     }
     return false;
--- lib/views/instruction_view.dart
+++ lib/views/instruction_view.dart
@@ -3,17 +3,15 @@ import 'package:guider/helpers/search.dart';
 import 'package:guider/objects/instruction.dart';
 import 'package:guider/objects/instruction_steps.dart';
 import 'package:guider/views/fullscreen_image_viewer.dart';
-import 'package:guider/views/instructionstep_view.dart';
+import 'package:guider/views/instructionstep_overview.dart';
 
 class InstructionView extends StatefulWidget {
   const InstructionView({super.key, required this.instruction});
 
   final InstructionElement instruction;
-  
-  @override
-    State<InstructionView> createState() => _InstructionViewState();
 
-  
+  @override
+  State<InstructionView> createState() => _InstructionViewState();
 }
 
 class _InstructionViewState extends State<InstructionView> {
@@ -25,13 +23,13 @@ class _InstructionViewState extends State<InstructionView> {
     getAllInstructionSteps();
   }
 
-  Future getAllInstructionSteps() async{
+  Future getAllInstructionSteps() async {
     var result = await Search.getInstructionSteps(widget.instruction.id);
     setState(() {
       _steps = result;
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,71 +38,75 @@ class _InstructionViewState extends State<InstructionView> {
         title: Text(widget.instruction.title),
       ),
       body: _steps != null
-      ? Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-              Text("Short title: ${widget.instruction.shortTitle}"),
-              const SizedBox(height: 10),
-              const Text("Description"),
-              buildDesc(),
-              buildImage(),
-              const SizedBox(height: 10),
-              buildButton(),
-          ],
-        ),
-      ) : const Text("Empty."),
+          ? Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text("Short title: ${widget.instruction.shortTitle}"),
+                  const SizedBox(height: 10),
+                  const Text("Description"),
+                  buildDesc(),
+                  buildImage(),
+                  const SizedBox(height: 10),
+                  buildButton(),
+                ],
+              ),
+            )
+          : const Text("Empty."),
     );
   }
 
   Widget buildDesc() => Expanded(
-    flex: 1,
-    // child: Scrollbar(
-      // thumbVisibility: true,
-      child: SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Text(widget.instruction.description),
-    ),
-    // ),
-    );
-
-    Widget buildImage() => Expanded(
-      flex: 1,
-      child: GestureDetector(
-      child: Hero(
-        tag: "imageHero",
-        child: Image.network(widget.instruction.image, fit: BoxFit.cover)),
-      onTap: () {
-      Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => FullScreenImageViewer(widget.instruction.image)),
-        );
-      },
-    ),
-  );
-
-    Widget buildButton() => ElevatedButton(
-      onPressed: () {
-        if(_steps!.isNotEmpty) {
-          Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => InstructionStepView(instructionStep: _steps![0], instructionTitle: widget.instruction.title,)),
-        );
-        }
-        else {
-          print("No instruction steps available.");
-        }
-      }, 
-      child: const Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("Instruction steps"),
-          Icon(
-            Icons.arrow_forward,
-            size: 24,
-          )
-        ],
-      ),
+        flex: 1,
+        // child: SafeArea(
+        //   child: Scrollbar(
+        // thumbVisibility: true,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Text(widget.instruction.description),
+        ),
+        //   ),
+        // ),
       );
 
+  Widget buildImage() => Expanded(
+        flex: 1,
+        child: GestureDetector(
+          child: Hero(
+              tag: "imageHero",
+              child:
+                  Image.network(widget.instruction.image, fit: BoxFit.cover)),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      FullScreenImageViewer(widget.instruction.image)),
+            );
+          },
+        ),
+      );
+
+  Widget buildButton() => Directionality(
+        textDirection: TextDirection.rtl,
+        child: ElevatedButton.icon(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            if (_steps!.isNotEmpty) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => InstructionStepOverview(
+                    instruction: widget.instruction,
+                    steps: _steps!,
+                  ),
+                ),
+              );
+            } else {
+              print("No instruction steps available.");
+            }
+          },
+          label: const Text("Instruction Steps"),
+        ),
+      );
 }
