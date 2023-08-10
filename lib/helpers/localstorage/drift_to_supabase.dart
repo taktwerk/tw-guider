a4acc9da70e3094f32ee300d8f80678fcb288ac1
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-10 14:33:30 +0200

    Upload of History on Sync

--- lib/helpers/localstorage/drift_to_supabase.dart
+++ lib/helpers/localstorage/drift_to_supabase.dart
@@ -1,3 +1,4 @@
+import 'package:guider/helpers/localstorage/key_value.dart';
 import 'package:guider/main.dart';
 import 'package:guider/objects/singleton.dart';
 
@@ -20,6 +21,28 @@ class DriftToSupabase {
         'updated_by': entry.updatedBy,
       });
       await Singleton().getDatabase().updateFeedback(entry);
+    }
+  }
+
+  static Future<void> uploadHistory() async {
+    var lastSynced = await KeyValue.getValue(KeyValueEnum.history.key);
+    print("Last synced $lastSynced");
+    var history = await Singleton()
+        .getDatabase()
+        .notSyncedHistoryEntries(DateTime.parse(lastSynced!));
+    print("Not synced history: $history");
+    int len = history.length;
+    for (int i = 0; i < len; i++) {
+      var historyEntry = history[i];
+      await supabase.from('history').upsert({
+        'user_id': historyEntry.userId,
+        'instruction_id': historyEntry.instructionId,
+        "created_at": historyEntry.createdAt.toIso8601String(),
+        "created_by": historyEntry.createdBy,
+        "updated_at": historyEntry.updatedAt.toIso8601String(),
+        "updated_by": historyEntry.updatedBy,
+        "instruction_step_id": historyEntry.instructionStepId,
+      }, onConflict: 'user_id,instruction_id').gt("updated_at", lastSynced);
     }
   }
 }
