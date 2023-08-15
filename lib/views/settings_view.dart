2455bac0c2e959870b1f5398e1d5ceb7eff937c0
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-15 14:25:40 +0200

    Change of language select widget in settings

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:guider/helpers/localstorage/localstorage.dart';
 import 'package:guider/languages/languages.dart';
+import 'package:guider/languages/supported_languages.dart';
 import 'package:guider/main.dart';
+import 'package:guider/objects/singleton.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -10,27 +13,72 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
+  Locale? selectedItem;
+  List<Locale> languages = SupportedLanguages.all;
+  List<Setting> settings = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getSetting();
+  }
+
+  Future<void> getSetting() async {
+    var result = await Singleton().getDatabase().getSettings(currentUser!);
+    setState(() {
+      settings = result;
+      if (result.isNotEmpty) {
+        selectedItem = Locale.fromSubtags(languageCode: result.first.language);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l = Languages.of(context);
-    return Column(
-      children: [
-        Text(l!.settingsTitle),
-        ElevatedButton(
-            onPressed: () {
-              GuiderApp.setLocale(
-                  context, const Locale.fromSubtags(languageCode: 'de'));
-              setState(() {});
-            },
-            child: const Text("DE")),
-        ElevatedButton(
-            onPressed: () {
-              GuiderApp.setLocale(
-                  context, const Locale.fromSubtags(languageCode: 'en'));
-              setState(() {});
-            },
-            child: const Text("EN"))
-      ],
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          SizedBox(
+            width: 300,
+            child: DropdownButtonFormField(
+              isExpanded: true,
+              icon: const Icon(Icons.arrow_drop_down_circle),
+              value: settings.isEmpty
+                  ? Locale.fromSubtags(languageCode: l!.languageCode)
+                  : selectedItem,
+              items: languages
+                  .map((item) => DropdownMenuItem<Locale>(
+                        value: item,
+                        child: Text(item.languageCode),
+                      ))
+                  .toList(),
+              onChanged: (item) => setState(
+                () {
+                  onClick(item);
+                  selectedItem = item;
+                },
+              ),
+              decoration: InputDecoration(
+                labelText: l!.languages,
+                prefixIcon: const Icon(Icons.format_list_numbered),
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void onClick(lang) async {
+    GuiderApp.setLocale(
+        context, Locale.fromSubtags(languageCode: lang.languageCode));
+    if (currentUser != null) {
+      await Singleton()
+          .getDatabase()
+          .updateUserSettings(currentUser!, lang.languageCode);
+    }
   }
 }
