b969a8e9ba05eef0f4ed3d306c5ba3eb8f675ad1
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-17 17:46:09 +0200

    Add StreamBuilder to Settings Page

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -18,54 +18,47 @@ class _SettingsViewState extends State<SettingsView> {
   List<Setting> settings = [];
 
   @override
-  void initState() {
-    super.initState();
-    getSetting();
-  }
-
-  Future<void> getSetting() async {
-    var result = await Singleton().getDatabase().getSettings(currentUser!);
-    setState(() {
-      settings = result;
-      if (result.isNotEmpty) {
-        selectedItem = Locale.fromSubtags(languageCode: result.first.language);
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    var settingsStream = Singleton().getDatabase().getSettings(currentUser!);
     final l = Languages.of(context);
     return Container(
       padding: const EdgeInsets.all(20),
       child: Column(
         children: [
-          SizedBox(
-            width: 300,
-            child: DropdownButtonFormField(
-              isExpanded: true,
-              icon: const Icon(Icons.arrow_drop_down_circle),
-              value: settings.isEmpty
-                  ? Locale.fromSubtags(languageCode: l!.languageCode)
-                  : selectedItem,
-              items: languages
-                  .map((item) => DropdownMenuItem<Locale>(
-                        value: item,
-                        child: Text(item.languageCode),
-                      ))
-                  .toList(),
-              onChanged: (item) => setState(
-                () {
-                  onClick(item);
-                  selectedItem = item;
-                },
-              ),
-              decoration: InputDecoration(
-                labelText: l!.languages,
-                prefixIcon: const Icon(Icons.format_list_numbered),
-                border: const OutlineInputBorder(),
-              ),
-            ),
+          StreamBuilder(
+            stream: settingsStream,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Setting>> snapshot) {
+              return SizedBox(
+                width: 300,
+                child: DropdownButtonFormField(
+                  isExpanded: true,
+                  icon: const Icon(Icons.arrow_drop_down_circle),
+                  value: snapshot.hasData
+                      ? snapshot.data!.isEmpty
+                          ? Locale.fromSubtags(languageCode: l!.languageCode)
+                          : Locale.fromSubtags(
+                              languageCode: snapshot.data!.first.language)
+                      : Locale.fromSubtags(languageCode: l!.languageCode),
+                  items: languages
+                      .map((item) => DropdownMenuItem<Locale>(
+                            value: item,
+                            child: Text(item.languageCode),
+                          ))
+                      .toList(),
+                  onChanged: (item) => setState(
+                    () {
+                      onClick(item);
+                    },
+                  ),
+                  decoration: InputDecoration(
+                    labelText: l!.languages,
+                    prefixIcon: const Icon(Icons.format_list_numbered),
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
