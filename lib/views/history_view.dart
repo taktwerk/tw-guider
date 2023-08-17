c58da4525ef8d0261847431b9bfde7b1669572ff
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-17 17:47:48 +0200

    Add StreamBuilder to History View

--- lib/views/history_view.dart
+++ lib/views/history_view.dart
@@ -1,63 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:guider/helpers/localstorage/localstorage.dart';
-import 'package:guider/languages/languages.dart';
 import 'package:guider/main.dart';
-import 'package:guider/objects/singleton.dart';
 import 'package:guider/widgets/listitem.dart';
 
-class HistoryView extends StatefulWidget {
+class HistoryView extends ConsumerStatefulWidget {
   const HistoryView({super.key});
 
   @override
-  State<StatefulWidget> createState() => _HistoryViewState();
+  ConsumerState<HistoryView> createState() => _HistoryViewState();
 }
 
-class _HistoryViewState extends State<HistoryView> {
-  List<Instruction>? _instructions;
+class _HistoryViewState extends ConsumerState<HistoryView>
+    with AutomaticKeepAliveClientMixin<HistoryView> {
   final ScrollController _scrollController = ScrollController();
 
   @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
-  Future getData() async {
-    if (currentUser != null) {
-      var data = await Singleton()
-          .getDatabase()
-          .getUserHistoryAsInstructions(currentUser!);
-      setState(() {
-        _instructions = data;
-      });
-    }
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+    final database = ref.watch(todoDBProvider);
+
+    var historyEntries = database.getUserHistoryAsInstructions(currentUser!);
     return Scaffold(
         body: Column(
       children: [
-        _instructions != null
-            ? _instructions!.isNotEmpty
-                ? Expanded(
-                    child: Scrollbar(
+        StreamBuilder(
+            stream: historyEntries,
+            builder: (BuildContext context,
+                AsyncSnapshot<List<Instruction>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.connectionState == ConnectionState.active ||
+                  snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return errorOrLoadingCard('🚨 Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  return Expanded(
+                      child: Scrollbar(
                     controller: _scrollController,
                     thumbVisibility: true,
                     child: ListView.builder(
                       controller: _scrollController,
                       physics: const BouncingScrollPhysics(),
-                      itemCount: _instructions!.length,
+                      itemCount: snapshot.data?.length,
                       itemBuilder: (context, index) {
-                        return ListItem(instruction: _instructions![index]);
+                        return ListItem(instruction: snapshot.data![index]);
                       },
                     ),
-                  ))
-                : Text(Languages.of(context)!.noHistoryAvailable)
-            : Center(
-                child: Text(Languages.of(context)!.noHistoryAvailable),
-              ),
+                  ));
+                } else {
+                  return const Text("Empty data");
+                }
+              } else {
+                return Text('State: ${snapshot.connectionState}');
+              }
+            })
       ],
     ));
+  }
+
+  Widget errorOrLoadingCard(input) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Card(
+          elevation: 4,
+          child: Center(
+            child: Text(input),
+          ),
+        ),
+      ),
+    );
   }
 }
