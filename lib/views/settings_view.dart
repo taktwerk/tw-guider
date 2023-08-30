7ccffaa3c87271400503705a99aca4b1d44a1df5
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-30 17:52:06 +0200

    Put settingsView content inside of a ListView

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -46,135 +46,154 @@ class _SettingsViewState extends State<SettingsView> {
   Widget build(BuildContext context) {
     var settingsStream = Singleton().getDatabase().getSettings(currentUser!);
     final l = Languages.of(context);
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Text("User $currentUser"),
-          StreamBuilder(
-            stream: settingsStream,
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Setting>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.connectionState == ConnectionState.active ||
-                  snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text('🚨 Error: ${snapshot.error}');
-                } else if (snapshot.hasData && snapshot.data != null) {
-                  return Column(
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: DropdownButtonFormField(
-                          isExpanded: true,
-                          icon: const Icon(Icons.arrow_drop_down_circle),
-                          value: snapshot.hasData
-                              ? snapshot.data!.isEmpty
-                                  ? Locale.fromSubtags(
-                                      languageCode: l!.languageCode)
+    return ListView(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  "User $currentUser",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              StreamBuilder(
+                stream: settingsStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Setting>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.connectionState ==
+                          ConnectionState.active ||
+                      snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Text('🚨 Error: ${snapshot.error}');
+                    } else if (snapshot.hasData && snapshot.data != null) {
+                      return Column(
+                        children: [
+                          SizedBox(
+                            width: 300,
+                            child: DropdownButtonFormField(
+                              isExpanded: true,
+                              icon: const Icon(Icons.arrow_drop_down_circle),
+                              value: snapshot.hasData
+                                  ? snapshot.data!.isEmpty
+                                      ? Locale.fromSubtags(
+                                          languageCode: l!.languageCode)
+                                      : Locale.fromSubtags(
+                                          languageCode:
+                                              snapshot.data!.first.language)
                                   : Locale.fromSubtags(
-                                      languageCode:
-                                          snapshot.data!.first.language)
-                              : Locale.fromSubtags(
-                                  languageCode: l!.languageCode),
-                          items: languages
-                              .map((item) => DropdownMenuItem<Locale>(
-                                    value: item,
-                                    child: Text(item.languageCode),
-                                  ))
-                              .toList(),
-                          onChanged: (item) => onLanguageChange(item),
-                          decoration: InputDecoration(
-                            labelText: l!.languages,
-                            prefixIcon: const Icon(Icons.format_list_numbered),
-                            border: const OutlineInputBorder(),
+                                      languageCode: l!.languageCode),
+                              items: languages
+                                  .map((item) => DropdownMenuItem<Locale>(
+                                        value: item,
+                                        child: Text(item.languageCode),
+                                      ))
+                                  .toList(),
+                              onChanged: (item) => onLanguageChange(item),
+                              decoration: InputDecoration(
+                                labelText: l!.languages,
+                                prefixIcon:
+                                    const Icon(Icons.format_list_numbered),
+                                border: const OutlineInputBorder(),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      SwitchListTile(
-                        title: Text(l.realtime),
-                        subtitle: Text(l.realtimeText),
-                        thumbIcon: thumbIcon,
-                        value: snapshot.data!.isEmpty
-                            ? false
-                            : snapshot.data!.first.realtime,
-                        onChanged: (bool value) {
-                          if (snapshot.data!.isNotEmpty) {
-                            onRealtimeChange(value);
-                          }
-                        },
-                      ),
-                      SwitchListTile(
-                        title: const Text("Lightmode/ Darkmode"),
-                        thumbIcon: themeIcon,
-                        value: snapshot.data!.isEmpty
-                            ? true
-                            : snapshot.data!.first.lightmode,
-                        onChanged: (bool value) {
-                          if (snapshot.data!.isNotEmpty) {
-                            onThemeChange(value);
-                          }
-                        },
-                      ),
-                    ],
-                  );
-                } else {
-                  return const Text("Empty data");
-                }
-              } else {
-                return Text('State: ${snapshot.connectionState}');
-              }
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: ElevatedButton.icon(
-                onPressed: () async {
-                  if (!kIsWeb) {
-                    await AppUtil.deleteAllImages();
+                          SwitchListTile(
+                            title: Text(l.realtime),
+                            subtitle: Text(l.realtimeText),
+                            thumbIcon: thumbIcon,
+                            value: snapshot.data!.isEmpty
+                                ? false
+                                : snapshot.data!.first.realtime,
+                            onChanged: (bool value) {
+                              if (snapshot.data!.isNotEmpty) {
+                                onRealtimeChange(value);
+                              }
+                            },
+                          ),
+                          SwitchListTile(
+                            title: const Text("Lightmode/ Darkmode"),
+                            thumbIcon: themeIcon,
+                            value: snapshot.data!.isEmpty
+                                ? true
+                                : snapshot.data!.first.lightmode,
+                            onChanged: (bool value) {
+                              if (snapshot.data!.isNotEmpty) {
+                                onThemeChange(value);
+                              }
+                            },
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Text("Empty data");
+                    }
+                  } else {
+                    return Text('State: ${snapshot.connectionState}');
                   }
-                  await Singleton().getDatabase().deleteEverything();
-                  await KeyValue.resetKeyValues();
-                  logger.w("Deleted everything");
                 },
-                icon: const Icon(Icons.delete),
-                label: const Text("DB")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: ElevatedButton.icon(
+                    onPressed: () async {
+                      if (!kIsWeb) {
+                        await AppUtil.deleteAllImages();
+                      }
+                      await Singleton().getDatabase().deleteEverything();
+                      await KeyValue.resetKeyValues();
+                      logger.w("Deleted everything");
+                    },
+                    icon: const Icon(Icons.delete),
+                    label: const Text("DB")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      final db = Singleton().getDatabase();
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DriftDbViewer(db)));
+                    },
+                    icon: const Icon(Icons.storage),
+                    label: const Text("DB")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: ElevatedButton.icon(
+                    style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.red)),
+                    onPressed: () async {
+                      logger.i("Logged out");
+                      currentUser = null;
+                      var prefs = await Singleton().getPrefInstance();
+                      prefs.remove(KeyValueEnum.currentUser.key);
+                      await KeyValue.saveLogin(false)
+                          .then((value) => Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginPage()),
+                              ));
+                    },
+                    icon: Transform.flip(
+                      flipX: true,
+                      child: const Icon(Icons.logout),
+                    ),
+                    label: const Text("Logout")),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: ElevatedButton.icon(
-                onPressed: () {
-                  final db = Singleton().getDatabase();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => DriftDbViewer(db)));
-                },
-                icon: const Icon(Icons.storage),
-                label: const Text("DB")),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: ElevatedButton.icon(
-                style: const ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll<Color>(Colors.red)),
-                onPressed: () async {
-                  logger.i("Logged out");
-                  currentUser = null;
-                  var prefs = await Singleton().getPrefInstance();
-                  prefs.remove(KeyValueEnum.currentUser.key);
-                  await KeyValue.saveLogin(false)
-                      .then((value) => Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LoginPage()),
-                          ));
-                },
-                icon: const Icon(Icons.logout),
-                label: const Text("Logout")),
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 
