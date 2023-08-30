be185b325cd09d590fc5b440a2904fdc3b4087df
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-30 16:49:28 +0200

    Remove of BoxShadow of Description

--- lib/views/instruction_view.dart
+++ lib/views/instruction_view.dart
@@ -245,22 +245,6 @@ class _InstructionViewState extends State<InstructionView> {
       flex: 1,
       child: Container(
         margin: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          // color: Colors.white,
-          borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-              bottomRight: Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: const Offset(0, 3), // changes position of shadow
-            ),
-          ],
-        ),
         child: Scrollbar(
           thumbVisibility: true,
           controller: _scrollController,
