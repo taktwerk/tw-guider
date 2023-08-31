1fb80c483538462009a65c12ae9f3146ba2b28a4
ksiarsky <k.siarsky@taktwerk.ch>
2023-08-31 11:31:48 +0200

    Remove of UserPopup

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:guider/helpers/localstorage/key_value.dart';
 import 'package:guider/helpers/localstorage/localstorage.dart';
 import 'package:guider/helpers/localstorage/realtime.dart';
 import 'package:guider/languages/languages.dart';
@@ -7,7 +6,6 @@ import 'package:guider/main.dart';
 import 'package:guider/objects/singleton.dart';
 import 'package:guider/views/history_view.dart';
 import 'package:guider/views/instruction_view.dart';
-import 'package:guider/views/login.dart';
 import 'package:guider/views/settings_view.dart';
 import 'package:guider/views/home_view.dart';
 
@@ -22,7 +20,6 @@ class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late TabController tabController;
   List<User>? users;
-  final usersStream = Singleton().getDatabase().allUserEntriesAsStream;
   Stream histStream = Realtime.getHistoryStream();
   List<Instruction>? openHistory;
   BuildContext? oldDialogContext;
@@ -113,7 +110,6 @@ class _MyHomePageState extends State<MyHomePage>
       appBar: AppBar(
         title: Text("${myTabs[tabController.index].text}"),
         bottom: TabBar(controller: tabController, tabs: myTabs),
-        // actions: [getUserPopup()],
       ),
       body: TabBarView(
         controller: tabController,
@@ -124,68 +120,5 @@ class _MyHomePageState extends State<MyHomePage>
         ],
       ),
     );
-  }
-
-  Widget getUserPopup() {
-    return StreamBuilder(
-        stream: usersStream,
-        builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.connectionState == ConnectionState.active ||
-              snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Text('🚨 Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              return PopupMenuButton(
-                itemBuilder: (context) => List.generate(
-                    snapshot.data!.length + 1,
-                    (index) => index != snapshot.data!.length
-                        ? PopupMenuItem(
-                            onTap: () {
-                              setState(() {
-                                currentUser = snapshot.data![index].id;
-                                KeyValue.setCurrentUser(
-                                    snapshot.data![index].id);
-                              });
-                            },
-                            child: Center(
-                                child:
-                                    Text("User ${snapshot.data![index].id}")),
-                          )
-                        : PopupMenuItem(
-                            child: Center(
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(Languages.of(context)!.logout),
-                                  const Icon(Icons.logout),
-                                ],
-                              ),
-                            ),
-                            onTap: () async {
-                              logger.i("Logged out");
-                              currentUser = null;
-                              var prefs = await Singleton().getPrefInstance();
-                              prefs.remove(KeyValueEnum.currentUser.key);
-                              await KeyValue.saveLogin(false)
-                                  .then((value) => Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const LoginPage()),
-                                      ));
-                            },
-                          )),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-              );
-            } else {
-              return const Text("Empty data");
-            }
-          } else {
-            return Text('State: ${snapshot.connectionState}');
-          }
-        });
   }
 }
