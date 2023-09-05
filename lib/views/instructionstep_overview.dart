00e8f95122c45ee420449d9ba62dae74aac288b7
ksiarsky <k.siarsky@taktwerk.ch>
2023-09-05 16:41:12 +0200

    Add of lastVisitedStep-Stream to InstructionStepOverview (fix of flickering screen on web)

--- lib/views/instructionstep_overview.dart
+++ lib/views/instructionstep_overview.dart
@@ -16,26 +16,7 @@ class InstructionStepOverview extends StatefulWidget {
 }
 
 class _InstructionStepViewState extends State<InstructionStepOverview> {
-  InstructionStep? selectedItem;
   StreamSubscription? _subscription;
-
-  @override
-  void initState() {
-    super.initState();
-    init();
-  }
-
-  void init() async {
-    _subscription = Singleton()
-        .getDatabase()
-        .getLastVisitedStep(
-            instructionId: widget.instruction.id, userId: currentUser!)
-        .listen((event) {
-      setState(() {
-        selectedItem = event.firstOrNull;
-      });
-    });
-  }
 
   @override
   void dispose() {
@@ -43,11 +24,10 @@ class _InstructionStepViewState extends State<InstructionStepOverview> {
     super.dispose();
   }
 
-  _renderWidget() {
-    return selectedItem != null
+  _renderWidget(step) {
+    return step != null
         ? InstructionStepView(
-            instructionTitle: widget.instruction.title,
-            instructionStep: selectedItem!)
+            instructionTitle: widget.instruction.title, instructionStep: step)
         : const CircularProgressIndicator();
   }
 
@@ -64,6 +44,8 @@ class _InstructionStepViewState extends State<InstructionStepOverview> {
     final steps = Singleton()
         .getDatabase()
         .getInstructionStepsByInstructionId(widget.instruction.id);
+    final lastVisitedStep = Singleton().getDatabase().getLastVisitedStep(
+        instructionId: widget.instruction.id, userId: currentUser!);
     return Scaffold(
         appBar: AppBar(
             title: SingleChildScrollView(
@@ -82,47 +64,71 @@ class _InstructionStepViewState extends State<InstructionStepOverview> {
                 snapshot.connectionState == ConnectionState.done) {
               if (snapshot.hasError) {
                 return Text('🚨 Error: ${snapshot.error}');
-              } else if (snapshot.hasData && selectedItem != null) {
-                return Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: DropdownButtonFormField(
-                          isExpanded: true,
-                          icon: const Icon(Icons.arrow_drop_down_circle),
-                          value: selectedItem,
-                          items: snapshot.data!
-                              .map((item) => DropdownMenuItem<InstructionStep>(
-                                    value: item,
-                                    child: Text("${l!.step} ${item.stepNr}"),
-                                  ))
-                              .toList(),
-                          onChanged: (item) => setState(
-                            () {
-                              if (item != null) {
-                                setNewStep(item.id);
-                                selectedItem = item;
-                              }
-                            },
+              } else if (snapshot.hasData) {
+                return StreamBuilder(
+                  stream: lastVisitedStep,
+                  builder: (BuildContext stepContext,
+                      AsyncSnapshot<List<InstructionStep>> stepSnapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                            ConnectionState.active ||
+                        snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('🚨 Error: ${snapshot.error}');
+                      } else if (stepSnapshot.hasData) {
+                        return Container(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 300,
+                                child: DropdownButtonFormField(
+                                  isExpanded: true,
+                                  icon:
+                                      const Icon(Icons.arrow_drop_down_circle),
+                                  value: stepSnapshot.data!.first,
+                                  items: snapshot.data!
+                                      .map((item) =>
+                                          DropdownMenuItem<InstructionStep>(
+                                            value: item,
+                                            child: Text(
+                                                "${l!.step} ${item.stepNr}"),
+                                          ))
+                                      .toList(),
+                                  onChanged: (item) {
+                                    if (item != null) {
+                                      setNewStep(item.id);
+                                    }
+                                  },
+                                  decoration: InputDecoration(
+                                    labelText: l!.instructionSteps,
+                                    prefixIcon:
+                                        const Icon(Icons.format_list_numbered),
+                                    border: const OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 5.0),
+                              Expanded(
+                                  child: ListView(
+                                children: [
+                                  _renderWidget(stepSnapshot.data!.first)
+                                ],
+                              )),
+                              backAndForthButtons(
+                                  snapshot.data, stepSnapshot.data!.first),
+                            ],
                           ),
-                          decoration: InputDecoration(
-                            labelText: l!.instructionSteps,
-                            prefixIcon: const Icon(Icons.format_list_numbered),
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5.0),
-                      Expanded(
-                          child: ListView(
-                        children: [_renderWidget()],
-                      )),
-                      backAndForthButtons(snapshot.data),
-                    ],
-                  ),
+                        );
+                      } else {
+                        return const Text("Empty data LASTVISITEDSTEP");
+                      }
+                    } else {
+                      return Text('State: ${snapshot.connectionState}');
+                    }
+                  },
                 );
               } else {
                 return const Text("Empty data OVERVIEW");
@@ -134,38 +140,32 @@ class _InstructionStepViewState extends State<InstructionStepOverview> {
         ));
   }
 
-  Widget backAndForthButtons(steps) {
+  Widget backAndForthButtons(steps, currentStep) {
     final l = Languages.of(context);
-    return selectedItem != null
+    return currentStep != null
         ? Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              steps.indexOf(selectedItem!) != 0
+              steps.indexOf(currentStep) != 0
                   ? ElevatedButton.icon(
                       icon: const Icon(Icons.arrow_back_ios),
                       onPressed: () {
-                        setState(() {
-                          var index =
-                              (steps.indexOf(selectedItem!) - 1) % steps.length;
-                          setNewStep(steps[index].id);
-                          selectedItem = steps[index];
-                        });
+                        var index =
+                            (steps.indexOf(currentStep!) - 1) % steps.length;
+                        setNewStep(steps[index].id);
                       },
                       label: Text(l!.back),
                     )
                   : Container(),
-              steps.indexOf(selectedItem!) != steps.length - 1
+              steps.indexOf(currentStep!) != steps.length - 1
                   ? Directionality(
                       textDirection: TextDirection.rtl,
                       child: ElevatedButton.icon(
                         icon: const Icon(Icons.arrow_back_ios),
                         onPressed: () {
-                          setState(() {
-                            var index = (steps.indexOf(selectedItem!) + 1) %
-                                steps.length;
-                            setNewStep(steps[index].id);
-                            selectedItem = steps[index];
-                          });
+                          var index =
+                              (steps.indexOf(currentStep!) + 1) % steps.length;
+                          setNewStep(steps[index].id);
                         },
                         label: Text(l!.next),
                       ),
