23f328877c3267f8a655fbf9479a281c26456925
ksiarsky <k.siarsky@taktwerk.ch>
2023-10-12 16:39:24 +0200

    Add VideoFileWidget & AudioFileWidget
    TWGA-24 Content types
    TWGA-25 Instruction Assets

--- lib/widgets/file_widgets.dart
+++ lib/widgets/file_widgets.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:guider/helpers/localstorage/localstorage.dart';
 import 'package:guider/languages/languages.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:media_kit_video/media_kit_video.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 Map<String, Function> fileTypeToWidget = {
@@ -36,6 +37,39 @@ class _FileWidgetState extends State<FileWidget> {
     return Container();
   }
 }
+
+class VideoFileWidget extends FileWidget {
+  const VideoFileWidget({super.key, required Asset asset})
+      : super(asset: asset);
+
+  @override
+  State<FileWidget> createState() => _VideoFileWidgetState();
+}
+
+class _VideoFileWidgetState extends _FileWidgetState {
+  late final player = Player();
+  // Create a [VideoController] to handle video output from [Player].
+  late final controller = VideoController(player);
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: make sure that the video file (url) exists
+    player.open(Media(widget.asset.file!));
+    final video = Video(
+      controller: controller,
+    );
+
+    return Center(
+      child: video,
+    );
+  }
+}
 class PdfFileWidget extends FileWidget {
   const PdfFileWidget({super.key, required Asset asset}) : super(asset: asset);
 
@@ -47,5 +81,111 @@ class _PdfFileWidgetState extends _FileWidgetState {
   @override
   Widget build(BuildContext context) {
     return SfPdfViewer.network(widget.asset.file ?? "");
+  }
+}
+
+class AudioFileWidget extends FileWidget {
+  const AudioFileWidget({super.key, required Asset asset})
+      : super(asset: asset);
+
+  @override
+  State<FileWidget> createState() => _AudioFileWidgetState();
+}
+
+class _AudioFileWidgetState extends _FileWidgetState {
+  late final player = Player();
+  Duration _position = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+  bool _playing = false;
+  StreamSubscription<Duration>? _positionStream;
+  StreamSubscription<Duration>? _durationStream;
+  bool currentlyChangingSlider = false;
+
+  StreamSubscription<bool>? _isPlayingStream;
+
+  @override
+  void dispose() async {
+    player.dispose();
+    _positionStream?.cancel();
+    _isPlayingStream?.cancel();
+    _durationStream?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeAudio();
+  }
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  Future<void> initializeAudio() async {
+    //TODO: make sure that the audio file (url) exists
+    await player.open(Media(widget.asset.file!), play: false);
+    _positionStream = player.stream.position.listen((e) {
+      //only change position of slider when the user is not actively using the slider
+      if (!currentlyChangingSlider) {
+        setState(() {
+          _position = e;
+        });
+      }
+    });
+    _durationStream = player.stream.duration.listen((e) {
+      setState(() {
+        _totalDuration = e;
+      });
+    });
+    _isPlayingStream = player.stream.playing.listen((e) {
+      setState(() {
+        _playing = e;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          IconButton(
+            icon: _playing
+                ? const Icon(Icons.pause)
+                : const Icon(Icons.play_arrow),
+            onPressed: () {
+              player.playOrPause();
+            },
+          ),
+          Slider(
+            value: _position.inSeconds.toDouble(),
+            min: 0,
+            max: _totalDuration.inSeconds.toDouble(),
+            onChanged: (value) {
+              setState(() {
+                _position = Duration(seconds: value.toInt());
+              });
+            },
+            onChangeStart: (value) {
+              setState(() {
+                currentlyChangingSlider = true;
+              });
+            },
+            onChangeEnd: (value) {
+              setState(() {
+                currentlyChangingSlider = false;
+              });
+              player.seek(Duration(seconds: value.toInt()));
+            },
+          ),
+          Text(
+              '${_printDuration(_position)} / ${_printDuration(_totalDuration)}'),
+        ],
+      ),
+    );
   }
 }
